c468e8a563ef95ca955e1a463d4b4237c8e1d221
pnield <49818225+pnield@users.noreply.github.com>
2019-04-22 17:14:08 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,207 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <stdio.h>
+#include <string>
+#include <windows.h>
+#include <tchar.h>
+
+#include "resource.h"
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+//Current displayed texture
+SDL_Texture* gTexture = NULL;
+
+Mix_Chunk* song = NULL;
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
+	{
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		success = false;
+	}
+
+	else
+	{
+		//Set texture filtering to linear
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+		{
+			printf("Warning: Linear texture filtering not enabled!");
+		}
+
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+			success = false;
+		}
+
+		else
+		{
+			//Create renderer for window
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+			if (gRenderer == NULL)
+			{
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if (!(IMG_Init(imgFlags) & imgFlags))
+				{
+					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+					success = false;
+				}
+
+				if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+				{
+					printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	HMODULE hModule = GetModuleHandle(_T("SDL Resource.exe"));
+	HRSRC hSprite = FindResource(hModule, MAKEINTRESOURCE(IDB_PNG1), _T("PNG"));
+	unsigned int sprite_size = SizeofResource(hModule, hSprite);
+	HGLOBAL hgSprite = LoadResource(hModule, hSprite);
+	unsigned char* sprite_data = (unsigned char*)LockResource(hgSprite);
+
+	SDL_Surface* sprite = IMG_Load_RW(SDL_RWFromConstMem(sprite_data, sprite_size), 1);
+	
+	//Load PNG texture
+	gTexture = SDL_CreateTextureFromSurface(gRenderer, sprite);
+
+	FreeResource(hgSprite);
+
+	HRSRC hSong = FindResource(hModule, MAKEINTRESOURCE(IDR_OGG1), _T("OGG"));
+	unsigned int song_size = SizeofResource(hModule, hSong);
+	HGLOBAL hgSong = LoadResource(hModule, hSong);
+	unsigned char* song_data = (unsigned char*)LockResource(hgSong);
+
+	song = Mix_LoadWAV_RW(SDL_RWFromConstMem(song_data, song_size), 1);
+	FreeResource(hgSong);
+
+	if (gTexture == NULL)
+	{
+		printf("Failed to load texture image!\n");
+		success = false;
+	}
+
+	if (song == NULL)
+	{
+		printf("Failed to load song!\n");
+		success = false;
+	}
+
+	return success;
+}
+
+void close()
+{
+	//Free loaded image
+	SDL_DestroyTexture(gTexture);
+	gTexture = NULL;
+
+	//Destroy window	
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	gRenderer = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+	//Start up SDL and create window
+	if (!init())
+		printf("init failed");
+
+	else
+	{
+		//Load media
+		if (!loadMedia())
+			printf("loadMedia failed");
+
+		else
+		{
+			Mix_PlayChannel(-1, song, 0);
+
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+
+			//While application is running
+			while (!quit)
+			{
+				//Handle events on queue
+				while (SDL_PollEvent(&e) != 0)
+				{
+					//User requests quit
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+				}
+
+				//Clear screen
+				SDL_RenderClear(gRenderer);
+
+				//Render texture to screen
+				SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+
+				//Update screen
+				SDL_RenderPresent(gRenderer);
+			}
+		}
+	}
+
+	//Free resources and close SDL
+	close();
+
+	return 0;
+}
